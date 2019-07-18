0a5dd084d930e8e5cafe65578b4dffcfb0a09df5
Zhou YY <767906331@qq.com>
2019-07-18 21:36:43 +0800

    注册登录的格式验证

--- bartertreasure/src/views/Login.vue
+++ bartertreasure/src/views/Login.vue
@@ -12,6 +12,7 @@
             clearable
             placeholder="请输入手机号"
             left-icon="phone"
+            @blur="checkPhone()"
           />
 
           <van-field
@@ -19,6 +20,7 @@
             type="password"
             placeholder="请输入密码"
             left-icon="lock"
+             @blur="check1()"
           />
           <van-button type="primary" round size="large" to="/index">登录</van-button>
           <span>
@@ -31,6 +33,7 @@
 
 <script>
 import axios from 'axios'
+import { Toast } from 'vant'
 export default {
   name:'Login',
   data(){
@@ -43,6 +46,22 @@ export default {
     toRegister(){
       this.$router.push('/register')
     },
+    check1(){
+      var testRePho = /^(\w){6,20}$/;
+      if(testRePho.test(this.password)){
+
+      }else{
+        Toast('密码格式不规范');
+      }
+    },
+    checkPhone(){
+      var testRePwd = /^1((3[\d])|(4[5,6,7,9])|(5[0-3,5-9])|(6[5-7])|(7[0-8])|(8[\d])|(9[1,8,9]))\d{8}$/;
+      if(testRePwd.test(this.phone)){
+
+      }else{
+        Toast('请输入正确的手机号');
+      }
+    }
   }
 }
 </script>
